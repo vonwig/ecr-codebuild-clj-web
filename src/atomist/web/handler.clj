4ff706f41d26f420a834eef2aa0a16aaa8ba8eed
Jim Clark <jim@atomist.com>
2022-01-03 21:48:18 -0800

    bump

--- src/atomist/web/handler.clj
+++ src/atomist/web/handler.clj
@@ -2,16 +2,14 @@
   (:require [compojure.core :refer [defroutes GET]]
             [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
-            [compojure.route :as route])
-  (:import [java.security KeyStore]
-           [java.io ByteArrayOutputStream FileInputStream]
-           [java.util Base64])
+            [compojure.route :as route]
+            [cljc.java-time.local-date :as ld])
   (:gen-class))
 
 (defroutes app
   (GET "/health" _ (constantly {:status 200 :body "ok"}))
   (->
-   (GET "/" _ (constantly {:status 200 :body {:hey "you" :version "v1"}}))
+   (GET "/" _ (constantly {:status 200 :body {:hey "you" :version 39 :time (str (ld/now))}}))
    (wrap-json-body {:keywords? true :bigdecimals? true})  
    (wrap-json-response))
   (route/not-found "<h1>not found</h1>"))
@@ -19,7 +17,7 @@
 (defn -main
   [& args]
   (try
-    (println "... run-jetty")
+    (println "... run-jetty with args" args)
     (run-jetty app {:port 3000})
     (catch Throwable t
       (println "failed to start " t))))
