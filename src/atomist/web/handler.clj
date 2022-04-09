1f9308c9f141fbe5b51fd72cff35c7cb1ae2937c
Jim Clark <jim@atomist.com>
2022-04-09 09:45:51 -0700

    next version -> 50

--- src/atomist/web/handler.clj
+++ src/atomist/web/handler.clj
@@ -9,7 +9,7 @@
 (defroutes app
   (GET "/health" _ (constantly {:status 200 :body "ok"}))
   (->
-   (GET "/" _ (constantly {:status 200 :body {:hey "you" :version 49 :time (str (ld/now))}}))
+   (GET "/" _ (constantly {:status 200 :body {:hey "you" :version 50 :time (str (ld/now))}}))
    (wrap-json-body {:keywords? true :bigdecimals? true})  
    (wrap-json-response))
   (route/not-found "<h1>not found</h1>"))
